225ee152ac03a0102534e22c565c03b454cea15c
thelisanjay2399 <134526859+thelisanjay@users.noreply.github.com>
2024-11-29 15:22:16 +0530

    new name

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './WelcomeScreen.css';
 
 const WelcomeScreen = () => {
-  const text = "WELCOME TO AWS TRAINING NITIN SANJAY IBRAHIM";
+  const text = "WELCOME TO AWS TRAINING Team";
   const [displayText, setDisplayText] = useState('');
 
   useEffect(() => {
